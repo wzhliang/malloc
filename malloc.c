64f0a84c5d83cd9d385a5d54e79799bacbfcadd2
Wenzhi Liang <wenzhi.liang@gmail.com>
2013-06-08 16:40:44 +0000

    multiple allocation works

--- malloc.c
+++ malloc.c
@@ -5,7 +5,7 @@
 
 typedef struct chunk_tag
 {
-    void *start;
+    unsigned char *addr;
     int free;
     int index;
 } chunk_t;
@@ -16,14 +16,17 @@ typedef struct chunk_tag
 #define HEAP_SIZE (CHUNK_SIZE*NUM_CHUNKS)
 #define MAX_FREE_BLOCKS 1024
 
-#define reset_chunk(b) (b)->start = 0; (b)->free = 1;
+/*#define reset_chunk(b) (b)->addr = 0; (b)->free = 1;*/
 
-void *the_heap[HEAP_SIZE];
+unsigned char the_heap[HEAP_SIZE];
 chunk_t the_chunks[NUM_CHUNKS];
+int free_list[MAX_FREE_BLOCKS]; // -1 means it's not occupied. other value stores the index of chunk that's free
 
 static void mark_chunks(int start, int len);
 static size_t sizeof_cont_chunkc(int ch);
 static int find_chunks(size_t size);
+static void free_list_add(int idx);
+static void free_list_remove(int idx);
 
 void init(void)
 {
@@ -31,19 +34,27 @@ void init(void)
 
     memset(the_heap, HEAP_SIZE, 0);
 
-    for (i = 0; i < MAX_FREE_BLOCKS; i++)
+    for (i = 0; i < NUM_CHUNKS; i++)
     {
-        the_chunks[i].start = the_heap + i* CHUNK_SIZE;
+        the_chunks[i].addr = the_heap + i* CHUNK_SIZE;
         the_chunks[i].free = 1;
         the_chunks[i].index = i;
-
-        reset_chunk(the_chunks+i);
     }
+
+    for (i = 0; i < MAX_FREE_BLOCKS; i++)
+    {
+        free_list[i] = -1;
+    }
+    free_list[0] = 0;
 }
 
 void *mmaloc(size_t size)
 {
     int ch;
+
+    /* FIXME: this is probably not right */
+    if ( size == 0 )
+        size = 1;
 
     ch = find_chunks(size);
     if ( ch < 0 )
@@ -53,11 +64,37 @@ void *mmaloc(size_t size)
 
     mark_chunks( ch, size / CHUNK_SIZE + 1);
 
-    return the_chunks[ch].start;
+    return (void*)(the_chunks[ch].addr);
 }
 
 void free(void *ptr)
 {
+}
+
+static void free_list_remove(int idx)
+{
+    int i;
+    for (i = 0; i < MAX_FREE_BLOCKS; i++)
+    {
+        if ( free_list[i] == idx )
+        {
+            free_list[i] = -1;
+            break;
+        }
+    }
+}
+
+static void free_list_add(int idx)
+{
+    int i;
+    for (i = 0; i < MAX_FREE_BLOCKS; i++)
+    {
+        if ( free_list[i] == -1 )
+        {
+            free_list[i] = idx;
+            break;
+        }
+    }
 }
 
 static void mark_chunks(int start, int len)
@@ -66,11 +103,13 @@ static void mark_chunks(int start, int len)
 
     assert( start < MAX_FREE_BLOCKS );
 
-    for ( i = 0; i < len; i++ )
+    for ( i = start; i < len; i++ )
     {
         assert( the_chunks[i].free == 1 );
         the_chunks[i].free = 0;
     }
+    free_list_remove(start);
+    free_list_add(start+len);
 }
 
 static size_t sizeof_cont_chunkc(int ch)
@@ -88,17 +127,20 @@ static size_t sizeof_cont_chunkc(int ch)
 
 static int find_chunks(size_t size)
 {
+    int head = -1;
     int i;
 
     for ( i = 0; i < MAX_FREE_BLOCKS; i ++ )
     {
-        if ( sizeof_cont_chunkc( i ) >= size )
+        if ( free_list[i] >= 0 && sizeof_cont_chunkc( free_list[i] ) >= size )
         {
-            return i;
+            head = free_list[i];
+            printf("Found chunk: %d.\n", head);
+            break;
         }
     }
 
-    return -1;
+    return head;
 }
 
 /* TESTS */
@@ -108,9 +150,10 @@ int test_1(void)
     size_t s;
     void *ptr[10];
 
+    memset( ptr, 0, 10 );
     for (i = 0; i < 10; i++)
     {
-        s = rand() % 4096;
+        s = rand() % 2*4096;
         ptr[i] = mmaloc( s );
         if ( ptr[i] == NULL )
         {
